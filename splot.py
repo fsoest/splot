6f20c64077f46247ea31f778728b614843cbd3dd
Felix <felix@soest-net.de>
2023-02-03 13:55:12 +0100

    Remove copx capability, fix levels+neighbours

--- splot.py
+++ splot.py
@@ -2,30 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from argparse import ArgumentParser
 from base import basemap, plot_neighbour, plot_current, get_sectors_with_copx, get_airways, get_fixes, header, get_sectors
-from pandas import concat
 import os
 
 
-def main(sis, annotate, scale, levels, group, neighbours, airways, copx_bool, waypoints, coloured, dotted):
+def main(sis, annotate, scale, levels, group, neighbours, airways, waypoints, coloured, dotted):
     sis = sis.split(',')
-    neighbours = neighbours.split(',')
-    dotted = dotted.split(',')
-    if copx_bool:
-        fig, ax = plt.subplots(nrows=1, ncols=2)
-        ax, ax1 = ax
-    else:
-        fig, ax = plt.subplots(nrows=1, ncols=1)
+    if neighbours is not None:
+        neighbours = neighbours.split(',')
+    if dotted is not None:
+        dotted = dotted.split(',')
+    fig, ax = plt.subplots(nrows=1, ncols=1)
 
     # Initialise Basemap for coordinate transformation
     m = basemap()
 
-    if levels is None:
-        min_level, max_level = 600, 0
-    else:
+    if levels is not None:
         min_level, max_level = levels
 
     # Import sectors from GNG file
-    sectors = get_sectors() # get_sectors_with_copx()
+    sectors = get_sectors()
     sel_sectors = []
     if waypoints is not None:
         fixes = get_fixes()
@@ -36,30 +31,17 @@ def main(sis, annotate, scale, levels, group, neighbours, airways, copx_bool, wa
             print('No sector found')
             return None
 
-    copx_list = []
+    def plot_main():
+        for key, val in sectors.items():
+            for i, si in enumerate(sis):
+                if group:
+                    if si in key:
+                        plot_current(val, ax, m, annotate, coloured, i)
+                else:
+                    if si == key:
+                        plot_current(val, ax, m, annotate, coloured, i)
 
-    # Plot main sector and calculate upper/lower levels
-    for key, val in sectors.items():
-        for i, si in enumerate(sis):
-            if group:
-                if si in key:
-                    plot_current(val, ax, m, False, coloured, i)
-                    sel_sectors.append(val.name)
-                    copx_list.append(val.copx_table(fixes, m, ax))
-                    # if levels is None:
-                    #     if val.lower_level < min_level:
-                    #         min_level = val.lower_level
-                    #     if val.upper_level > max_level:
-                    #         max_level = val.upper_level
-            else:
-                if si == key:
-                    plot_current(val, ax, m, False, coloured, i)
-                    copx_list.append(val.copx_table(fixes, m, ax))
-                    # if levels is None:
-                    #     if val.lower_level < min_level:
-                    #         min_level = val.lower_level
-                    #     if val.upper_level > max_level:
-                    #         max_level = val.upper_level
+    plot_main()
 
     # Axis plot limits
     x_min, x_max = ax.get_xlim()
@@ -68,55 +50,43 @@ def main(sis, annotate, scale, levels, group, neighbours, airways, copx_bool, wa
     y_shift = np.abs((y_max - y_min) / 2)
     ax.set_xlim((x_min - scale * x_shift, x_max + scale * x_shift))
     ax.set_ylim((y_min - scale * y_shift, y_max + scale * y_shift))
+    ax.set_aspect('equal', adjustable='box')
+    ax.axis(False)
 
     # Plot neighbouring sectors
     if levels is not None:
         for key, val in sectors.items():
             for si in sis:
+                is_key_in_dotted = key in dotted if dotted is not None else False
+                is_key_in_neighbours = key in neighbours if neighbours is not None else False
                 if group:
                     if (si not in key) and ((
                             min_level < val.lower_level < max_level or min_level < val.upper_level < max_level)
-                                            or (key in neighbours) or (key in dotted)):
-                        plot_neighbour(val, ax, m, key in neighbours, key in dotted)
+                                            or is_key_in_neighbours or is_key_in_dotted):
+                        plot_neighbour(val, ax, m, is_key_in_neighbours, is_key_in_dotted)
                 else:
                     if si != key and ((min_level < val.lower_level < max_level or min_level < val.upper_level < max_level)
-                            or (key in neighbours)):
-                        plot_neighbour(val, ax, m, key in neighbours, key in dotted)
-    for key, val in sectors.items():
-        for neighbour in neighbours:
-            if neighbour in key:
-                plot_neighbour(val, ax, m, key in neighbours)
-        for dot in dotted:
-            if dot in key:
-                plot_neighbour(val, ax, m, key in neighbours, True)
+                            or is_key_in_neighbours):
+                        plot_neighbour(val, ax, m, is_key_in_neighbours, is_key_in_dotted)
+    else:
+        if neighbours is not None:
+            for key, val in sectors.items():
+                for neighbour in neighbours:
+                    if neighbour in key:
+                        plot_neighbour(val, ax, m, key in neighbours)
+        if dotted is not None:
+            for dot in dotted:
+                if dot in key:
+                    plot_neighbour(val, ax, m, key in neighbours, True)
 
-    # Plot main sector again
-    for key, val in sectors.items():
-        for i, si in enumerate(sis):
-            if group:
-                if si in key:
-                    plot_current(val, ax, m, annotate, coloured, i)
-            else:
-                if si == key:
-                    plot_current(val, ax, m, annotate, coloured, i)
+    #Plot main sector again
+    plot_main()
+
     if airways is not None:
         airways = airways.split(',')
         airway_dict = get_airways()
         for airway in airways:
             airway_dict[airway].plot(ax, m)
-
-    ax.set_aspect('equal', adjustable='box')
-    ax.axis(False)
-
-    if copx_list and copx_bool:
-        ax1.axis(False)
-        copx = concat(copx_list, ignore_index=True)
-        for index, row in copx.iterrows():
-            if row['to'] in sel_sectors:
-                copx.drop(index, inplace=True)
-        copx.sort_values('fix', inplace=True)
-        table = ax1.table(cellText=copx.values, colLabels=header, loc='center')
-        table.scale(1, 0.5)
 
     if waypoints is not None:
         waypoints = waypoints.split(',')
@@ -146,13 +116,12 @@ if __name__ == '__main__':
                                                                    ' Sector otherwise only plots sectors with'
                                                                    ' given IDs')
     parser.add_argument('-n', '--neighbours', type=str, help='Sector IDs of neighbouring sectors for which to'
-                                                             ' add labels, either e.g. GIN1,GIN2 or GIN,TAU', default='')
+                                                             ' add labels, either e.g. GIN1,GIN2 or GIN,TAU', default=None)
     parser.add_argument('-w', '--airways', type=str, help='Airways to plot, separated by ,', default=None)
-    parser.add_argument('-c', '--copx', action='store_true', help='Add Copx table to plot')
     parser.add_argument('-y', '--waypoints', type=str, help='Waypoints to plot, separated by ,', default=None)
     parser.add_argument('-f', '--coloured', action='store_true', help='If set, sectors have different colours')
-    parser.add_argument('-d', '--dotted', type=str, help='Sector IDs (GIN1,GIN2 or GIN,TAU) of sectors to plot with dashed lines', default='')
+    parser.add_argument('-d', '--dotted', type=str, help='Sector IDs (GIN1,GIN2 or GIN,TAU) of sectors to plot with dashed lines', default=None)
 
     args = parser.parse_args()
-    main(args.sectors, args.annotate, args.scale, args.levels, args.group, args.neighbours, args.airways, args.copx,
+    main(args.sectors, args.annotate, args.scale, args.levels, args.group, args.neighbours, args.airways,
          args.waypoints, args.coloured, args.dotted)
